8b3bdc75a0372b3f7c2644bafd7f59eb3963475b
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2024-09-22 17:54:14 +0400

    added animation when loading data from API

--- src/components/converter/converter.jsx
+++ src/components/converter/converter.jsx
@@ -7,15 +7,19 @@ export const Converter = ({ onConverting, result }) => {
 	const [currencyTo, setCurrencyTo] = useState('RUB');
 	const [sum, setSum] = useState(100);
 	const [currenciesCode, setСurrenciesCode] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
+		setIsLoading(true);
 		fetch('https://v6.exchangerate-api.com/v6/500eda8129cb286bbd60c0ad/latest/USD')
 			.then((response) => response.json())
-			.then((data) => setСurrenciesCode(Object.keys(data.conversion_rates)));
+			.then((data) => setСurrenciesCode(Object.keys(data.conversion_rates)))
+			.finally(() => setIsLoading(false));
 	}, []);
 
 	return (
 		<div className={styles.converter}>
+			{isLoading && <div className={styles.loader}></div>}
 			<div className={styles.conversion}>
 				<div className={styles.title}>У меня есть</div>
 				<div className={styles.select}>
